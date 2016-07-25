a643e7714607d86d9aa8ca7c8af0c28add5f0167
fazil-ahamath <fazilcruser1219@gmail.com>
2016-07-25 13:55:26 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class vowels {
+	public static void main(String[] args) {
+		String a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.next();
+		if(a.contains("a"))
+		{
+			System.out.println("Ya it is vowel");
+		}
+		
+		else if(a.contains("e"))
+		{
+			System.out.println("Ya it is vowel");
+		}
+		
+		else if(a.contains("i"))
+		{
+			System.out.println("Ya it is vowel");
+		}
+		
+		else	if(a.contains("o"))
+		{
+			System.out.println("Ya it is vowel");
+		}
+	
+		else 	if(a.contains("u"))
+		{
+			System.out.println("Ya it is vowel");
+		}
+		else{
+			System.out.println("it is constant");
+		}
+		
+	}
+}

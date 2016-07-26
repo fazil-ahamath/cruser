78580615eab8e291b31d122d3a0b6a55e8de39c7
fazil-ahamath <fazilcruser1219@gmail.com>
2016-07-26 12:36:52 +0530

    Add files via upload

---
+++ longest.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+public class longest {
+public static String LongestCommonPrefix(String[] strs) 
+		{
+    		if(strs==null || strs.length==0)
+    		{
+        		return "";
+    		}
+ 
+    		if(strs.length==1) 
+        		return strs[0];
+ 
+            int minLen = Integer.MAX_VALUE;
+
+    		for(String str: strs)
+    		{
+        		if(minLen > str.length())
+        		{
+            		minLen = str.length();
+        		}
+    		}
+ 
+    		for(int i=0; i<minLen; i++)
+    		{
+        		for(int j=0; j<strs.length-1; j++)
+        		{
+            		String s1 = strs[j];
+            		String s2 = strs[j+1];
+            		if(s1.charAt(i)!=s2.charAt(i))
+            		{
+                		return s1.substring(0, i);
+              		}
+        		}
+    		}
+ 
+	    	return strs[0].substring(0, minLen);
+		}	
+
+	public static void main(String[] args) throws IOException
+	{
+        char choice;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Input  Values(yes/no): ");
+        choice = (char)bf.read();
+        
+        if(choice != 'y')
+        {
+            bf = new BufferedReader(new InputStreamReader(System.in));    
+            String[] array = bf.readLine().split("\\s");
+            System.out.println(LongestCommonPrefix(array));                
+        }
+        else
+        {
+            String[] array = {"foo","foobar","foobarfoo","foobarbar"}; 
+            System.out.println(LongestCommonPrefix(array));
+        }            
+	}	
+}

2b6a6975fbe6c01a9c2c816b78ea747ea449e1b7
fazil-ahamath <fazilcruser1219@gmail.com>
2016-07-27 10:25:16 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class math{
+	public static void main(String[] args) {
+		double n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextDouble();
+		System.out.println(Math.round(n));
+
+		
+	}
+}
+

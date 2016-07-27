cc6bb01e8bb6d5fba020d8733aed6da37c985eb9
fazil-ahamath <fazilcruser1219@gmail.com>
2016-07-27 09:53:37 +0530

    Add files via upload

---
+++ neww.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class neww {
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		n=(int)(Math.random()*n+1);
+		System.out.println(n);
+		
+	}
+}
+
